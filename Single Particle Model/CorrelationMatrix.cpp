63c2ed1fdf1d329faaa8e9633fb601aae35a0f07
ChenYen Lai <chengyanlai@gmail.com>
2016-06-29 14:51:40 -0700

    add Makefile, fix the correlation matrix buiding subroutine, and some small changes.

--- Single Particle Model/CorrelationMatrix.cpp
+++ Single Particle Model/CorrelationMatrix.cpp
@@ -24,31 +24,16 @@ void CorrMat::BuildCorrMat(const Hamiltonian & H)
 {
     C_mat = MatrixXcd::Zero(Nsite, Nsite);
     B_mat = MatrixXcd::Zero(Nsite, Nsite);
-    
-    //cout << "Pre Corr Assign: \n" << C_mat << endl;
-    complex<double> sum;
-    for( int i = 0; i < Nsite; i++)
-    {
-        for(int j = 0; j < Nsite; j++)
-        {
-            sum = 0.;
-            for(int k = 0; k < Np; k++)
-            {
-                sum += H.EVec(i,k)*H.EVec(j,k);
-                
-            }
-            if ( sum.real() <= .0000000001)
-            {
-                sum = 0.;
-            }
-            //cout << "sum: " << sum.real() << endl;
-            C_mat(i,j) = sum;
-        }
+
+    MatrixXd Mat = MatrixXd::Zero(Nsite, Nsite);
+    for(int k = 0; k < Np; k++){
+      VectorXd vec = H.EVec.col(k);
+      Mat += vec * vec.transpose();
     }
-    
+    C_mat = Mat.cast <complex<double> > ();
 //    cout << "C_mat initial: \n" << C_mat(0,0).real() << endl;
 //    cout << "C_mat initial: \n" << C_mat(12,12).real() << endl;
-    
+
 }
 
 void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, double N_it, double Phi_max, double t_p)
@@ -59,19 +44,19 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
     MatrixXcd k4(Nsite, Nsite);
     MatrixXcd Ck_mat(Nsite, Nsite);
     MatrixXcd Cmat_new(Nsite, Nsite);
-    
+
     double t = 0;
     double Phi_t = 0;
-    
+
     int flag = 0;
-    
+
     Matrix2cd T_mat;
     complex<double> t1;//t_{j-1}
     complex<double> t2;//t_j
     complex<double> t3;//t_i
     complex<double> t4;//t_{i-1}
-    
-    
+
+
     for(int it = 0; it <= N_it; it++)
     {
         t = it*dt;
@@ -84,19 +69,19 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
             Phi_t = Phi_max;
         }
         //cout << "t: " << t << " Phi: " << Phi_t << endl;
-        
+
         T_mat(0,0) = h.J1*exp(I*Phi_t);//tbar_{j-1},tbar_i only need this information for if loop
         T_mat(0,1) = h.J2*exp(I*Phi_t);
         T_mat(1,0) = h.J1*exp(-I*Phi_t);//tbar_j, tbar_i-1
         T_mat(1,1) = h.J2*exp(-I*Phi_t);
         //cout << "Problem with Tmat\n";
-        
-        
-        
+
+
+
         for(int i = 0; i < Nsite; i++)//so begins evaluation of k1
             //double loop for each k vector
         {
-            
+
 
             for(int j = 0; j < Nsite; j++)//only doing upper triangle
             {
@@ -117,7 +102,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t1 = 0.;
                     //cout << "t1 is zero "<< j-1 <<endl;
                 }
-                
+
                 if((j >= 0) && (j < (Nsite - 1)))//t_j
                 {
                     if( j%2 == 0)
@@ -135,7 +120,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t2 = 0.;
                     //cout << "t2 is zero "<< j <<endl;
                 }
-                
+
                 if((i >= 0) && (i < (Nsite - 1)))//t_i
                 {
                     if( i%2 == 0)
@@ -171,15 +156,15 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                 }
                 k1(i,j) = dt*I*(t1+t2-t3-t4);
                 Ck_mat(i,j) = C_mat(i,j) + (1./2.)*k1(i,j);
-                
+
                 //cout << "k1 is set.\n";
             }
         }//end k1 loop onto k2
-        
+
         for(int i = 0; i < Nsite; i++)//so begins evaluation of k2
             //double loop for each k vector
         {
-            
+
             for(int j = 0; j < Nsite; j++)//only doing upper triangle
             {
                 if(((j-1) >= 0) && ((j-1) < (Nsite - 1)))//t_{j-1}
@@ -199,7 +184,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t1 = 0.;
                     //cout << "t1 is zero "<< j-1 <<endl;
                 }
-                
+
                 if((j >= 0) && (j < (Nsite - 1)))//t_j
                 {
                     if( j%2 == 0)
@@ -217,7 +202,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t2 = 0.;
                     //cout << "t2 is zero "<< j <<endl;
                 }
-                
+
                 if((i >= 0) && (i < (Nsite - 1)))//t_i
                 {
                     if( i%2 == 0)
@@ -256,11 +241,11 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                 //cout << "k2 is set.\n";
             }
         }//end k2 loop
-        
+
         for(int i = 0; i < Nsite; i++)//so begins evaluation of k3
             //double loop for each k vector
         {
-            
+
             for(int j = 0; j < Nsite; j++)//only doing upper triangle
             {
                 if(((j-1) >= 0) && ((j-1) < (Nsite - 1)))//t_{j-1}
@@ -280,7 +265,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t1 = 0.;
                     //cout << "t1 is zero "<< j-1 <<endl;
                 }
-                
+
                 if((j >= 0) && (j < (Nsite - 1)))//t_j
                 {
                     if( j%2 == 0)
@@ -298,7 +283,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t2 = 0.;
                     //cout << "t2 is zero "<< j <<endl;
                 }
-                
+
                 if((i >= 0) && (i < (Nsite - 1)))//t_i
                 {
                     if( i%2 == 0)
@@ -334,15 +319,15 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                 }
                 k3(i,j) = dt*I*(t1+t2-t3-t4);
                 Ck_mat(i,j) = C_mat(i,j) + k3(i,j);
-                
+
                 //cout << "k3 is set.\n";
             }
         }
-        
+
         for(int i = 0; i < Nsite; i++)//so begins evaluation of k4
             //double loop for each k vector
         {
-            
+
             for(int j = 0; j < Nsite; j++)//only doing upper triangle
             {
                 if(((j-1) >= 0) && ((j-1) < (Nsite - 1)))//t_{j-1}
@@ -362,7 +347,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t1 = 0.;
                     //cout << "t1 is zero "<< j-1 <<endl;
                 }
-                
+
                 if((j >= 0) && (j < (Nsite - 1)))//t_j
                 {
                     if( j%2 == 0)
@@ -380,7 +365,7 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
                     t2 = 0.;
                     //cout << "t2 is zero "<< j <<endl;
                 }
-                
+
                 if((i >= 0) && (i < (Nsite - 1)))//t_i
                 {
                     if( i%2 == 0)
@@ -421,32 +406,33 @@ void CorrMat::RKPeierlsOnCMat(const Hamiltonian& h, ofstream &fout, double dt, d
         Cmat_new = C_mat+(1./6.)*k1+(1./3.)*k2+(1./3.)*k3+(1./6.)*k4;
         C_mat = Cmat_new;
         //cout << "new Corr mat is set.\n";
-        
+
         if(flag == 1000)
         {
             for(int i = 0; i < Nsite; i++)
             {
                 fout << i << " " << C_mat(i,i).real() << endl;
             }
-            
+
             //cout <<"Phi: " << Phi_t<< "Cmat evolve: \n"<< C_mat.real() << endl;
             fout << endl;
             flag = 0;
         }
-        
+
         flag++;
         //prepare output formula tomorrow
         T_mat.setZero();
 
     }
-    
+
     fout.close();
     //for 1D chain there are no nearest neigbors. THis has been taken care of in the equation
 }
 
-void CorrMat::RKAdiabatic(Hamiltonian & ham, ofstream &fout, ofstream &Fout, double dt, double N_it, double T, double J1, double J2, double h_0)
+void CorrMat::RKAdiabatic(Hamiltonian & ham, ofstream &fout, ofstream &Fout,
+  const double dt, const double N_it, const double T, const double J_0,
+  const double delta_0, const double h_0)
 {
-    
     MatrixXcd k1(Nsite, Nsite);
     MatrixXcd k2(Nsite, Nsite);
     MatrixXcd k3(Nsite, Nsite);
@@ -456,34 +442,34 @@ void CorrMat::RKAdiabatic(Hamiltonian & ham, ofstream &fout, ofstream &Fout, dou
     MatrixXd H1 = MatrixXd::Zero(Nsite,Nsite);
     MatrixXd H2 = MatrixXd::Zero(Nsite,Nsite);//used for H3 as well
     MatrixXd H4 = MatrixXd::Zero(Nsite,Nsite);
-    
-    double delta_0 = 0.5;//T \approx 50
+
     double delta;
-    double J_0 = 1.;
+    double J1, J2;
     //double h_0 = 1.;
     double h;
     //double omega = (2* 3.14159265)/T;
-    double omega = (8*atan(1.0))/T;
+    const double omega = (8*atan(1.0))/T;
 
     //double h_0;
-    
+
     double t1,t2,t4;
     //ham.FriendHam();
-    
+
     for(int it = 0; it <= N_it; it++)
     {
         if(it == 0 || it == N_it/2. || it == N_it)
         {
-            
-            ham.Diagonalize_Hamiltonian(ham.Friendly_Ham);//Should I diagonalize H(t) or H(t+dt)?
-            //I chose H(t+dt) because Cij(t+dt)
-            cout << "Setting Energy \n";
-            SetEnergyMat(ham);
-            int q = (Nsite/2)+1;
+            std::cout << t1 << " " << t2 << " " << t4 << std::endl;
+            std::cout << it << " " << J1 << " " << J2 << " " << h << std::endl;
+            // ham.Diagonalize_Hamiltonian(ham.Friendly_Ham);//Should I diagonalize H(t) or H(t+dt)?
+            // //I chose H(t+dt) because Cij(t+dt)
+            // cout << "Setting Energy \n";
+            // SetEnergyMat(ham);
+            // int q = (Nsite/2)+1;
             //complex<double> E = sqrt(conj(B_mat(q,q))*B_mat(q,q));
-            
-            Fout << J1 << " " << B_mat(q,q).real() << endl;
-            
+
+            // Fout << J1 << " " << B_mat(q,q).real() << endl;
+
             for(int i = 0; i < Nsite; i++)
             {
                 fout << i << " " << C_mat(i,i).real() << endl;
@@ -494,13 +480,12 @@ void CorrMat::RKAdiabatic(Hamiltonian & ham, ofstream &fout, ofstream &Fout, dou
             //            cout << "Hamiltonians 2 \n" << H2 << endl;
             //            cout << "Hamiltonians 4 \n" << H4 << endl;
             cout << " C_mat\n" << C_mat << endl;
-            
+
             //cout << ham.Friendly_Ham << endl;
-            
+
         }
-        
+
         t1 = it*dt;
-        //cout << "T1: " << t1 << endl;
         t2 = t1 + (0.5*dt);
         t4 = t1 + dt;
 
@@ -510,39 +495,39 @@ void CorrMat::RKAdiabatic(Hamiltonian & ham, ofstream &fout, ofstream &Fout, dou
             h = h_0*sin(omega*t1);
             H1 = ham.Thouless_Hamiltonian(J1, J2, h);//to get H(t)
 
-        
+
         k1 = dt* I*(C_mat*H1 - H1*C_mat);//k1 = f(t,y)->H(t)
-        Ck_mat = C_mat + (1./2.)*k1;
-        
+        Ck_mat = C_mat + 0.50e0 * k1;
+
             delta = delta_0*cos(omega*(t2));
             J1 = J_0 + delta;
             J2 = J_0 - delta;
             h = h_0*sin(omega*(t2));
             H2 = ham.Thouless_Hamiltonian(J1, J2, h); //to get H(t+dt)
-        
+
         k2 = dt* I*(Ck_mat*H2 - H2*Ck_mat);//k2 = f(t+1/2dt, y+1/2k1)
-        Ck_mat = C_mat + (1./2.)*k2;
-            
+        Ck_mat = C_mat + 0.50e0 * k2;
+
         k3 = dt* I*(Ck_mat*H2 - H2*Ck_mat);//k3 = f(t+1/2dt, y+1/2k2)
         Ck_mat = C_mat + k3;
-        
+
             delta = delta_0*cos(omega*t4);
             J1 = J_0 + delta;
             J2 = J_0 - delta;
             h = h_0*sin(omega*t4);
             H4 = ham.Thouless_Hamiltonian(J1, J2, h);//to get H(t)
-        
+
         k4 = dt*I*(Ck_mat*H4 - H4*Ck_mat);//k3 = f(t+dt, y+k3)-> H(t+dt)
-        
+
         Cmat_new = C_mat+(1./6.)*k1+(1./3.)*k2+(1./3.)*k3+(1./6.)*k4;
         C_mat = Cmat_new;
-        
 
 
-        
+
+
     }
     //end RK4
-    
+
     fout.close();
 }
 
@@ -552,28 +537,28 @@ void CorrMat::SetEnergyMat(const Hamiltonian& h)
     MatrixXcd EVec_adj;
     //cout << "DOn't xHAve complex Eiegen Mat!\n";
     EVec_c = h.EVec.cast<std::complex<double>>();
-    
+
     EVec_adj = EVec_c.adjoint();
     complex<double> sum= 0.;
-    
+
     B_mat = EVec_c*C_mat*EVec_adj;
     for(int i = 0; i < Nsite; i++)
     {
         sum+= B_mat(i,i).real();
     }
-    
+
     //cout << "B_mat: \n" << B_mat << endl;
     //cout << "Sum of B_mat: \n" << sum << endl;
-    
+
     C_mat = EVec_adj*B_mat*EVec_c;
     sum = 0.0;
     for(int i = 0; i < Nsite; i++)
     {
         sum+= C_mat(i,i).real();
     }
-    
+
     //cout << "Sum of C matrix: \n" << sum << endl;
-    
+
 }
 
 //void CorrMat::OnsiteDensity(ofstream &fout)
@@ -583,7 +568,3 @@ void CorrMat::SetEnergyMat(const Hamiltonian& h)
 //        fout << i << " " << C_mat(i,i) << endl;
 //    }
 //}
-
-
-
-
--- Single Particle Model/CorrelationMatrix.h
+++ Single Particle Model/CorrelationMatrix.h
@@ -15,31 +15,34 @@ using namespace Eigen;
 class CorrMat
 {
 private:
-    
+
     MatrixXcd B_mat;
     MatrixXcd C_mat;
-    
+
     int Nsite;
     int Np;
-    
+
     complex<double> I;
     complex<double> tbar;
-    
+
 protected:
-    
+
 public:
-    
+
     CorrMat(const Hamiltonian&){};
     void GetNumberVariables(int _Np, int _Nsite);
     void SetEnergyMat(const Hamiltonian& h);
     void BuildCorrMat(const Hamiltonian&);
-    void RKPeierlsOnCMat(const Hamiltonian&, ofstream &fout, double dt, double N_it, double Phi_max, double t_p);
-    void RKAdiabatic(Hamiltonian& ham, ofstream &fout, ofstream &Fout, double dt, double N_it, double T, double J1, double J2, double h_0);
+    void RKPeierlsOnCMat(const Hamiltonian&, ofstream &fout, double dt,
+      double N_it, double Phi_max, double t_p);
+    void RKAdiabatic(Hamiltonian& ham, ofstream &fout, ofstream &Fout,
+      const double dt, const double N_it, const double T, const double J_0,
+      const double delta0, const double h_0);
     void ClearHam();
-    
+
     //void RKThouless(const Hamiltonian&, ofstream &fout, double dt, double N_it, double Phi_max, double t_p);
     //void OnsiteDensity(ofstream &fout);
-    
+
 };
 
 #endif /* CorrelationMatrix_h */
